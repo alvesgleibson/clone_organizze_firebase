3fb28759e2e212de4e28b617acf5bb58c5a679c5
Gleibson Alves <alvesgleibson@gmail.com>
2021-08-18 23:53:37 -0300

    Saving expense movement in Firebase

--- app/src/main/java/com/alvesgleibson/organizzeclonefirebase/project/activity/ExpenseActivity.java
+++ app/src/main/java/com/alvesgleibson/organizzeclonefirebase/project/activity/ExpenseActivity.java
@@ -1,16 +1,157 @@
 package com.alvesgleibson.organizzeclonefirebase.project.activity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.alvesgleibson.organizzeclonefirebase.R;
+import com.alvesgleibson.organizzeclonefirebase.project.entities.FinancialMovementUser;
+import com.alvesgleibson.organizzeclonefirebase.project.entities.User;
+import com.alvesgleibson.organizzeclonefirebase.project.helper.Base64Custom;
+import com.alvesgleibson.organizzeclonefirebase.project.helper.DateCustom;
+import com.alvesgleibson.organizzeclonefirebase.project.setting.SettingInstanceFirebase;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class ExpenseActivity extends AppCompatActivity {
 
+    private TextInputEditText ettInputUserExpenseDate2, ettInputUserExpenseDescription2, ettInputUserExpenseCategory2;
+    private EditText ettInputUserExpenseValue2;
+    private DatabaseReference myDatabaseReference = SettingInstanceFirebase.getInstanceFirebaseDatabase();
+    private FirebaseAuth myFirebaseAuth = SettingInstanceFirebase.getInstanceFirebaseAuthMethod();
+    private Double expenseAll, generalValue;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expense);
+
+        ettInputUserExpenseDate2 = findViewById(R.id.ettInputUserExpenseDate);
+        ettInputUserExpenseCategory2 = findViewById(R.id.ettInputUserExpenseCategory);
+        ettInputUserExpenseDescription2 = findViewById(R.id.ettInputUserExpenseDescription);
+        ettInputUserExpenseValue2 = findViewById(R.id.ettInputUserExpenseValue);
+
+        ettInputUserExpenseDate2.setText( DateCustom.getDateCurrent() );
+
+        recoveryIncomeAll();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void saveExpenseMovement(View view) {
+
+        validValueField();
+
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void validValueField() {
+
+        String valueUserInput = ettInputUserExpenseValue2.getText().toString();
+        String dateUser = ettInputUserExpenseDate2.getText().toString();
+        String category = ettInputUserExpenseCategory2.getText().toString();
+        String descriptionUser = ettInputUserExpenseDescription2.getText().toString();
+
+        if (!valueUserInput.isEmpty()) {
+            if (!dateUser.isEmpty()) {
+                if (!category.isEmpty()) {
+                    if (!descriptionUser.isEmpty()) {
+                        Double par = Double.parseDouble(valueUserInput);
+
+                        FinancialMovementUser movementUser = new FinancialMovementUser(category, dateUser, descriptionUser, "d", par);
+                        Double updatedExpense = par + expenseAll;
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                        builder.setCancelable( false );
+                        builder.setTitle("Salvar Dispesa?");
+                        builder.setMessage("Deseja realmente salvar essa dispesa? \n\n"+"Valor: R$ -"+par+"\nCategoria: "+category+"\nDescrição: " +descriptionUser+"\nCom data: "+dateUser);
+                        builder.setIcon(android.R.drawable.ic_menu_save);
+                        builder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                                updateExpenseFirebase( updatedExpense );
+                                movementUser.saveMovementIncomeFirebase();
+
+                                clearFields();
+                            }
+                        }).setNegativeButton("Não", null);
+                        builder.create().show();
+
+
+
+
+                    } else {
+                        Toast.makeText(this, "Informe valor para Descrição", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(this, "Informe valor para Categoria", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(this, "Informe valor para Data", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "Informe valor R$ para Cadastra", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void recoveryIncomeAll() {
+
+        String emailId = Base64Custom.encodeBase64(myFirebaseAuth.getCurrentUser().getEmail());
+        DatabaseReference referenceUserData = myDatabaseReference.child("Users").child(emailId);
+
+        referenceUserData.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+                expenseAll = user.getExpenseAll();
+                generalValue = user.getGolAll();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void updateExpenseFirebase(Double expense) {
+
+        String emailId = Base64Custom.encodeBase64(myFirebaseAuth.getCurrentUser().getEmail());
+        DatabaseReference referenceUserData = myDatabaseReference.child("Users").child(emailId);
+        referenceUserData.child("expenseAll").setValue(-expense);
+        Double valueSum = (generalValue - expense);
+        referenceUserData.child("golAll").setValue(valueSum);
+
+
+
+    }
+
+    public void clearFields(){
+
+        ettInputUserExpenseCategory2.setText("");
+        ettInputUserExpenseDescription2.setText("");
+        ettInputUserExpenseValue2.setText("");
+        Toast.makeText(this, "Despesa cadastrada com sucesso!!!", Toast.LENGTH_SHORT).show();
+
     }
 }
--- app/src/main/java/com/alvesgleibson/organizzeclonefirebase/project/activity/IncomeActivity.java
+++ app/src/main/java/com/alvesgleibson/organizzeclonefirebase/project/activity/IncomeActivity.java
@@ -1,30 +1,42 @@
 package com.alvesgleibson.organizzeclonefirebase.project.activity;
 
+import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.alvesgleibson.organizzeclonefirebase.R;
 import com.alvesgleibson.organizzeclonefirebase.project.entities.FinancialMovementUser;
+import com.alvesgleibson.organizzeclonefirebase.project.entities.User;
+import com.alvesgleibson.organizzeclonefirebase.project.helper.Base64Custom;
 import com.alvesgleibson.organizzeclonefirebase.project.helper.DateCustom;
+import com.alvesgleibson.organizzeclonefirebase.project.setting.SettingInstanceFirebase;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class IncomeActivity extends AppCompatActivity {
 
     private TextInputEditText txtDate, txtDescription, txtCategory;
     private EditText eTInputUser;
-    private DatabaseReference databaseReference;
-   // private FinancialMovementUser movementUser;
+    private DatabaseReference myDatabaseReference = SettingInstanceFirebase.getInstanceFirebaseDatabase();
+    private FirebaseAuth myFirebaseAuth = SettingInstanceFirebase.getInstanceFirebaseAuthMethod();
+    private Double incomeAll, generalValue;
 
 
 
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +48,8 @@ public class IncomeActivity extends AppCompatActivity {
         eTInputUser = findViewById(R.id.txtUserIncome);
 
         txtDate.setText(DateCustom.getDateCurrent());
+
+        recoveryIncomeAll();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -47,41 +61,103 @@ public class IncomeActivity extends AppCompatActivity {
 
 
     @RequiresApi(api = Build.VERSION_CODES.O)
-    public FinancialMovementUser validValueField(){
+    public void validValueField(){
 
-        String date = txtDate.getText().toString();
+        String valueUserInput = eTInputUser.getText().toString();
+        String dateUser = txtDate.getText().toString();
         String category = txtCategory.getText().toString();
-        String description = txtDescription.getText().toString();
-        String s = eTInputUser.getText().toString();
+        String descriptionUser = txtDescription.getText().toString();
+
+        if ( !valueUserInput.isEmpty() ){
+            if ( !dateUser.isEmpty()){
+                if ( !category.isEmpty()){
+                    if ( !descriptionUser.isEmpty()){
+                        Double par = Double.parseDouble( valueUserInput );
+
+                        FinancialMovementUser movementUser = new FinancialMovementUser( category, dateUser, descriptionUser, "r", par );
+                        Double updatedIncome = par + incomeAll;
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                        builder.setCancelable( false );
+                        builder.setTitle("Salvar Receita?");
+                        builder.setMessage("Deseja realmente salvar receita \n\n"+"Valor: R$ "+par+"\nCategoria :"+category+"\nDescrição: " +descriptionUser+"\nCom data: "+dateUser);
+                        builder.setIcon( R.drawable.ic_save_alert_dialog_24);
+                        builder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                updateIncomeFirebase( updatedIncome );
+                                movementUser.saveMovementIncomeFirebase( );
+
+                                clearFields();
+                            }
+                        }).setNegativeButton("Não", null);
+                        builder.create().show();
 
 
-
-        if ( !date.isEmpty() ){
-            if ( !category.isEmpty()){
-                if ( !description.isEmpty()){
-                    if ( !s.isEmpty()){
-
-                        FinancialMovementUser movementUser = new FinancialMovementUser( category, date, description, "r", Double.parseDouble( s ));
-
-                        movementUser.saveMovementIncomeFirebase( );
 
                     }else {
-                        Toast.makeText(this, "Informe valor para Cadastrar", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(this, "Informe valor para Descrição", Toast.LENGTH_SHORT).show();
                     }
                 }else {
-                    Toast.makeText(this, "Informe valor para Descrição", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Informe valor para Categoria", Toast.LENGTH_SHORT).show();
                 }
             }else {
-                Toast.makeText(this, "Informe valor para Categoria", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Informe valor para Data", Toast.LENGTH_SHORT).show();
             }
         }else {
-            Toast.makeText(this, "Informe valor para data", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Informe valor R$ para Cadastra", Toast.LENGTH_SHORT).show();
         }
 
 
-        return null;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void recoveryIncomeAll(){
 
+        String emailId = Base64Custom.encodeBase64(myFirebaseAuth.getCurrentUser().getEmail());
+        DatabaseReference referenceUserData = myDatabaseReference.child("Users").child( emailId );
+
+        referenceUserData.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue( User.class );
+                incomeAll = user.getIncomeAll();
+                generalValue = user.getGolAll();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void updateIncomeFirebase(Double income){
+
+        String emailId = Base64Custom.encodeBase64(myFirebaseAuth.getCurrentUser().getEmail());
+        DatabaseReference referenceUserData = myDatabaseReference.child("Users").child( emailId );
+        referenceUserData.child("incomeAll").setValue( income );
+        Double valueSum = (generalValue + income);
+        referenceUserData.child("golAll").setValue( valueSum );
+
+
+    }
+
+    public void clearFields(){
+
+        txtCategory.setText("");
+        txtDescription.setText("");
+        eTInputUser.setText("");
+        Toast.makeText(this, "Receita cadastrado com sucesso!!!", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void Alert(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+    }
 
 }
